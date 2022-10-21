8f804b3369a403e56f7ee67db7e0450871d4e6f5
Muhammad Ali <muhammad.ali4@ucalgary.ca>
2022-10-21 09:27:23 -0600

    added font, updates to overall application

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
+import 'dart:collection';
+// import 'package:calculator_app/resultant_form.dart';
+import 'package:calculator_app/suggestion_button.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+// class SuggestionButtonList extends StatefulWidget {
+//   const SuggestionButtonList({super.key});
+
+//   @override
+//   State<SuggestionButtonList> createState() => _SuggestionButtonListState();
+// }
+
+// class _SuggestionButtonListState extends State<SuggestionButtonList> {
+//   List<SuggestionButton> = [
+//     SuggestionButton()
+//   ]
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,21 +27,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        home: const MyHomePage(
+      title: '',
+    ));
+    // return MaterialApp(
+    //   title: 'Flutter Demo',
+    //   theme: ThemeData(
+    //     // This is the theme of your application.
+    //     //
+    //     // Try running your application with "flutter run". You'll see the
+    //     // application has a blue toolbar. Then, without quitting the app, try
+    //     // changing the primarySwatch below to Colors.green and then invoke
+    //     // "hot reload" (press "r" in the console where you ran "flutter run",
+    //     // or simply save your changes to "hot reload" in a Flutter IDE).
+    //     // Notice that the counter didn't reset back to zero; the application
+    //     // is not restarted.
+    //     primarySwatch: Colors.blue,
+    //   ),
+    //   home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    // );
   }
 }
 
@@ -48,17 +68,37 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<String> acceptableValues = [
+    "1",
+    "2",
+    "3",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "x",
+    "*",
+    "/",
+    "-",
+    "=",
+    "+",
+    "^",
+    "(",
+    ")",
+    ""
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  late var _addNumberToQueue = int.parse(resultantController.text);
+  Queue recentHistory = Queue();
+  final resultantController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    resultantController.dispose();
+    super.dispose();
   }
 
   @override
@@ -74,106 +114,117 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height * (heightScaleFactor),
-        color: Colors.grey.shade200,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            SizedBox(
-              width: MediaQuery.of(context).size.width * (7 / 24),
-              height: MediaQuery.of(context).size.height * (heightScaleFactor),
-              child: IconButton(
-                iconSize: 50,
-                icon: const Icon(
-                  Icons.menu_rounded,
-                ),
-                onPressed: () {},
-              ),
-            ),
-            SizedBox(
-              width:
-                  MediaQuery.of(context).size.width * (dividerWidthScaleFactor),
-              child: const Text(
-                "|",
-                textAlign: TextAlign.end,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w100,
-                ),
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width *
-                  (suggestionWidthScaleFactor),
-              height: MediaQuery.of(context).size.height * (heightScaleFactor),
-              child: TextButton(
+        body: Column(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height * (heightScaleFactor),
+          color: Colors.grey.shade200,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              SizedBox(
+                width: MediaQuery.of(context).size.width * (7 / 24),
+                height:
+                    MediaQuery.of(context).size.height * (heightScaleFactor),
+                child: IconButton(
+                  iconSize: 50,
+                  icon: const Icon(
+                    Icons.menu_rounded,
+                  ),
                   onPressed: () {},
-                  child: const Text(
-                    "500",
-                    style: TextStyle(
-                        color: Colors.grey,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20),
-                  )),
-            ),
-            SizedBox(
-              width:
-                  MediaQuery.of(context).size.width * (dividerWidthScaleFactor),
-              child: Text(
-                "|",
-                textAlign: TextAlign.end,
-                style: TextStyle(color: Colors.grey.shade700, fontSize: 20),
-              ),
-            ),
-            SizedBox(
-                width: MediaQuery.of(context).size.width *
-                    (suggestionWidthScaleFactor),
-                height:
-                    MediaQuery.of(context).size.height * (heightScaleFactor),
-                child: TextButton(
-                    onPressed: () {}, child: const Text("Suggestion"))),
-            SizedBox(
-              width:
-                  MediaQuery.of(context).size.width * (dividerWidthScaleFactor),
-              child: const Text(
-                "|",
-                textAlign: TextAlign.end,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w100,
                 ),
               ),
-            ),
-            SizedBox(
+              SizedBox(
                 width: MediaQuery.of(context).size.width *
-                    (suggestionWidthScaleFactor),
-                height:
-                    MediaQuery.of(context).size.height * (heightScaleFactor),
-                child: TextButton(
-                    onPressed: () {}, child: const Text("Suggestion"))),
-            SizedBox(
-              width:
-                  MediaQuery.of(context).size.width * (dividerWidthScaleFactor),
-              child: const Text(
-                "|",
-                textAlign: TextAlign.end,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w100,
+                    (dividerWidthScaleFactor),
+                child: const Text(
+                  "|",
+                  textAlign: TextAlign.end,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w100,
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
+              SizedBox(
                 width: MediaQuery.of(context).size.width *
                     (suggestionWidthScaleFactor),
                 height:
                     MediaQuery.of(context).size.height * (heightScaleFactor),
                 child: TextButton(
-                    onPressed: () {}, child: const Text("Suggestion"))),
-          ],
+                    onPressed: () {},
+                    child: const Text(
+                      "500",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    )),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width *
+                    (dividerWidthScaleFactor),
+                child: Text(
+                  "|",
+                  textAlign: TextAlign.end,
+                  style: TextStyle(color: Colors.grey.shade700, fontSize: 20),
+                ),
+              ),
+              SizedBox(
+                  width: MediaQuery.of(context).size.width *
+                      (suggestionWidthScaleFactor),
+                  height:
+                      MediaQuery.of(context).size.height * (heightScaleFactor),
+                  child: TextButton(
+                      onPressed: () {}, child: const Text("Suggestion"))),
+              SizedBox(
+                width: MediaQuery.of(context).size.width *
+                    (dividerWidthScaleFactor),
+                child: const Text(
+                  "|",
+                  textAlign: TextAlign.end,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w100,
+                  ),
+                ),
+              ),
+              SizedBox(
+                  width: MediaQuery.of(context).size.width *
+                      (suggestionWidthScaleFactor),
+                  height:
+                      MediaQuery.of(context).size.height * (heightScaleFactor),
+                  child: TextButton(
+                      onPressed: () {}, child: const Text("Suggestion"))),
+              SizedBox(
+                width: MediaQuery.of(context).size.width *
+                    (dividerWidthScaleFactor),
+                child: const Text(
+                  "|",
+                  textAlign: TextAlign.end,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w100,
+                  ),
+                ),
+              ),
+              SizedBox(
+                  width: MediaQuery.of(context).size.width *
+                      (suggestionWidthScaleFactor),
+                  height:
+                      MediaQuery.of(context).size.height * (heightScaleFactor),
+                  child: TextButton(
+                      onPressed: () {}, child: const Text("Suggestion"))),
+            ],
+          ),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        TextField(
+          style: const TextStyle(fontFamily: "Roboto"),
+          keyboardType: TextInputType.text,
+          textAlign: TextAlign.end,
+          controller: resultantController,
+        )
+      ],
+    ));
   }
 }
--- lib/suggestion_button.dart
+++ lib/suggestion_button.dart
@@ -1,28 +1,31 @@
 import 'package:flutter/material.dart';
 
-class SuggestionButton extends StatefulWidget {
-  const SuggestionButton({super.key});
+// class SuggestionButton extends StatefulWidget {
+//   const SuggestionButton({super.key});
 
-  @override
-  State<SuggestionButton> createState() => _SuggestionButtonState();
-}
+//   @override
+//   State<SuggestionButton> createState() => _SuggestionButtonState();
+// }
 
-class _SuggestionButtonState extends State<SuggestionButton> {
-  double heightScaleFactor = 1 / 10;
-  double suggestionWidthScaleFactor = 1 / 6;
+// class _SuggestionButtonState extends State<SuggestionButton> {
+//   double heightScaleFactor = 1 / 10;
+//   double suggestionWidthScaleFactor = 1 / 6;
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * (suggestionWidthScaleFactor),
-      height: MediaQuery.of(context).size.height * (heightScaleFactor),
-      child: TextButton(
-          onPressed: () {},
-          child: const Text(
-            "",
-            style: TextStyle(
-                color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 20),
-          )),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//       width: MediaQuery.of(context).size.width * (suggestionWidthScaleFactor),
+//       height: MediaQuery.of(context).size.height * (heightScaleFactor),
+//       child: TextButton(
+//           onPressed: () {},
+//           child: const Text(
+//             "",
+//             style: TextStyle(
+//                 color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 20),
+//           )),
+//     );
+//   }
+// }
+class SuggestionButton {
+  SuggestionButton();
 }
