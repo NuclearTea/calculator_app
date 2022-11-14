bca89dd14089e2c02c7eeead8f235ed9307c5fe8
Muhammad Ali <muhammad.ali4@ucalgary.ca>
2022-11-13 17:47:42 -0700

    Updated Visuals

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import "package:math_expressions/math_expressions.dart";
 import "package:function_tree/function_tree.dart";
+import 'package:circular_buffer/circular_buffer.dart';
+// import 'package:calculator_app/suggestion_button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -62,9 +64,9 @@ evaluateExpression(String equation) {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<String> acceptableValues = [
-    "C",
     "(",
     ")",
+    "^",
     "/",
     "7",
     "8",
@@ -103,7 +105,13 @@ class _MyHomePageState extends State<MyHomePage> {
   String equalButton = "=";
   String decimalPoint = ".";
 
-  Queue recentHistory = Queue();
+  // Queue recentHistory = Queue();
+  // final recentHistory = CircularBuffer<String>(4);
+  // List<int> recentHistory = [];
+  List<String> recentHistory = List<int>.filled(4, 0).map((e) => "").toList();
+  // List<String> recentHistory = [];
+  // var recentHistory = "";
+  // List<String> recentHistory = List<String>.filled(4, " asdf");
   var displayString = "";
   var equation = "";
 
@@ -143,104 +151,25 @@ class _MyHomePageState extends State<MyHomePage> {
                 color: Color(primaryDark),
               ),
               height: MediaQuery.of(context).size.height * (heightScaleFactor),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * (7 / 24),
-                    height: MediaQuery.of(context).size.height *
-                        (heightScaleFactor),
-                    child: IconButton(
-                      iconSize: 50,
-                      icon: const Icon(
-                        Icons.menu_rounded,
-                        color: Colors.white70,
-                      ),
-                      onPressed: () {},
-                    ),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width *
-                        (dividerWidthScaleFactor),
-                    child: const Text(
-                      "|",
-                      textAlign: TextAlign.end,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w100,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width *
-                        (suggestionWidthScaleFactor),
-                    height: MediaQuery.of(context).size.height *
-                        (heightScaleFactor),
-                    child: TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          "500",
-                          style: TextStyle(
-                              color: Colors.grey,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20),
-                        )),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width *
-                        (dividerWidthScaleFactor),
-                    child: Text(
-                      "|",
-                      textAlign: TextAlign.end,
-                      style:
-                          TextStyle(color: Colors.grey.shade700, fontSize: 20),
-                    ),
-                  ),
-                  SizedBox(
-                      width: MediaQuery.of(context).size.width *
-                          (suggestionWidthScaleFactor),
-                      height: MediaQuery.of(context).size.height *
-                          (heightScaleFactor),
-                      child: TextButton(
-                          onPressed: () {}, child: const Text("Suggestion"))),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width *
-                        (dividerWidthScaleFactor),
-                    child: const Text(
-                      "|",
-                      textAlign: TextAlign.end,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w100,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                      width: MediaQuery.of(context).size.width *
-                          (suggestionWidthScaleFactor),
-                      height: MediaQuery.of(context).size.height *
-                          (heightScaleFactor),
-                      child: TextButton(
-                          onPressed: () {}, child: const Text("Suggestion"))),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width *
-                        (dividerWidthScaleFactor),
-                    child: const Text(
-                      "|",
-                      textAlign: TextAlign.end,
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w100,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                      width: MediaQuery.of(context).size.width *
-                          (suggestionWidthScaleFactor),
-                      height: MediaQuery.of(context).size.height *
-                          (heightScaleFactor),
-                      child: TextButton(
-                          onPressed: () {}, child: const Text("Suggestion"))),
+              // child: manualAppBar(heightScaleFactor: heightScaleFactor, dividerWidthScaleFactor: dividerWidthScaleFactor, suggestionWidthScaleFactor: suggestionWidthScaleFactor),
+              child: AppBar(
+                backgroundColor: Color(primaryLight),
+                actions: [
+                  suggestionButton(Color(secondaryDark), recentHistory, 0),
+                  suggestionButton(Color(secondaryDark), recentHistory, 1),
+                  suggestionButton(Color(secondaryDark), recentHistory, 2),
+                  suggestionButton(Color(secondaryDark), recentHistory, 3),
+                  // suggestionButton()
+
+                  //   ElevatedButton(
+                  //       onPressed: () => print("suggestion button 2 pressed"),
+                  //       child: Text(recentHistory[1].toString())),
+                  //   ElevatedButton(
+                  //       onPressed: () => print("suggestion button 3 pressed"),
+                  //       child: Text(recentHistory[2].toString())),
+                  //   ElevatedButton(
+                  //       onPressed: () => print("suggestion button 4 pressed"),
+                  //       child: Text(recentHistory[3].toString())),
                 ],
               ),
             ),
@@ -248,7 +177,7 @@ class _MyHomePageState extends State<MyHomePage> {
               flex: 1,
               child: Container(
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
+                  // borderRadius: BorderRadius.circular(12),
                   color: Color(secondaryDefault),
                 ),
                 child: Row(
@@ -297,7 +226,7 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Container(
                 decoration: BoxDecoration(
                   color: Color(primaryLight),
-                  borderRadius: BorderRadius.circular(12),
+                  // borderRadius: BorderRadius.circular(12),
                 ),
                 width: double.infinity,
                 child: Row(
@@ -340,8 +269,8 @@ class _MyHomePageState extends State<MyHomePage> {
               margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
               decoration: BoxDecoration(
                   borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30)),
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20)),
                   color: Color(primaryDark)),
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -357,21 +286,24 @@ class _MyHomePageState extends State<MyHomePage> {
                     children: acceptableValues
                         .map(
                           (value) => Container(
-                            margin: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(100)),
+                            margin: const EdgeInsets.fromLTRB(5, 5, 5, 0),
+                            // decoration: BoxDecoration(
+                            //     borderRadius: BorderRadius.circular(100)),
                             child: ElevatedButton(
                               style: ElevatedButton.styleFrom(
-                                  backgroundColor: Color(primaryLight)),
+                                  backgroundColor: Color(primaryLight),
+                                  shape: const RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(20)))),
                               onPressed: () {
                                 setState(() {
-                                  if (value == clear) {
-                                    displayString = "";
-                                  } else if (value == equalButton) {
+                                  if (value == equalButton) {
                                     equation = displayString;
                                     var answer =
                                         displayString.replaceAll('x', "*");
                                     displayString =
+                                        evaluateExpression(answer).toString();
+                                    recentHistory[0] =
                                         evaluateExpression(answer).toString();
                                   } else {
                                     displayString += value;
@@ -399,6 +331,157 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+    );
+  }
+
+  Expanded suggestionButton(Color color, List<String> list, int index) {
+    return Expanded(
+      child: TextButton(
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Color(secondaryDark)),
+              shape:
+                  MaterialStateProperty.all(const ContinuousRectangleBorder())),
+          onPressed: () {
+            setState(() {
+              displayString = displayString + list[index];
+            });
+            print("suggestion button ${index + 1} pressed");
+          },
+          child: Text(
+            list[index],
+            style: const TextStyle(
+                fontFamily: "Roboto",
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+                color: Colors.white54),
+          )),
+    );
+  }
+
+  /*
+    Returns index of which suggetstion button should be repopulated next
+  */
+  int whichButton(List<String> list) {
+    // first check if there are any empty suggestion button through the list of recent history
+    bool fullList;
+    // int indexOfEmptyButton;
+    for (int i = 0; i < list.length; i++) {
+      if (list[i] == "") {
+        // emptyButton = true;
+        // indexOfEmptyButton = i;
+        return i;
+      }
+    }
+
+    fullList = true;
+
+    return 0;
+  }
+}
+
+class manualAppBar extends StatelessWidget {
+  const manualAppBar({
+    Key? key,
+    required this.heightScaleFactor,
+    required this.dividerWidthScaleFactor,
+    required this.suggestionWidthScaleFactor,
+  }) : super(key: key);
+
+  final double heightScaleFactor;
+  final double dividerWidthScaleFactor;
+  final double suggestionWidthScaleFactor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        SizedBox(
+          width: MediaQuery.of(context).size.width * (7 / 24),
+          height: MediaQuery.of(context).size.height * (heightScaleFactor),
+          child: IconButton(
+            iconSize: 50,
+            icon: const Icon(
+              Icons.menu_rounded,
+              color: Colors.white70,
+            ),
+            onPressed: () {},
+          ),
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * (dividerWidthScaleFactor),
+          child: const Text(
+            "|",
+            textAlign: TextAlign.end,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w100,
+            ),
+          ),
+        ),
+        SizedBox(
+          width:
+              MediaQuery.of(context).size.width * (suggestionWidthScaleFactor),
+          height: MediaQuery.of(context).size.height * (heightScaleFactor),
+          child: TextButton(
+              onPressed: () {},
+              child: const Text(
+                "500",
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              )),
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * (dividerWidthScaleFactor),
+          child: Text(
+            "|",
+            textAlign: TextAlign.end,
+            style: TextStyle(color: Colors.grey.shade700, fontSize: 20),
+          ),
+        ),
+        SizedBox(
+            width: MediaQuery.of(context).size.width *
+                (suggestionWidthScaleFactor),
+            height: MediaQuery.of(context).size.height * (heightScaleFactor),
+            child:
+                TextButton(onPressed: () {}, child: const Text("Suggestion"))),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * (dividerWidthScaleFactor),
+          child: const Text(
+            "|",
+            textAlign: TextAlign.end,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w100,
+            ),
+          ),
+        ),
+        SizedBox(
+            width: MediaQuery.of(context).size.width *
+                (suggestionWidthScaleFactor),
+            height: MediaQuery.of(context).size.height * (heightScaleFactor),
+            child:
+                TextButton(onPressed: () {}, child: const Text("Suggestion"))),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * (dividerWidthScaleFactor),
+          child: const Text(
+            "|",
+            textAlign: TextAlign.end,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w100,
+            ),
+          ),
+        ),
+        SizedBox(
+            width: MediaQuery.of(context).size.width *
+                (suggestionWidthScaleFactor),
+            height: MediaQuery.of(context).size.height * (heightScaleFactor),
+            child:
+                TextButton(onPressed: () {}, child: const Text("Suggestion"))),
+      ],
     );
   }
 }
--- lib/suggestion_button.dart
+++ lib/suggestion_button.dart
@@ -1,32 +1,71 @@
 // import 'package:flutter/material.dart';
+// import 'package:flutter/src/widgets/container.dart';
+// import 'package:flutter/src/widgets/framework.dart';
 
 // class SuggestionButton extends StatefulWidget {
-//   final Function update;
-//   const SuggestionButton({super.key, required this.update});
+//   final Color color;
+  
+//   final List<String> list;
+  
+//   final int index;
+  
+//   late final String displayString;
+//  get getDisplayString => this.displayString;
+
+//  set setDisplayString(final displayString) => this.displayString = displayString;
+
+//   SuggestionButton({
+//     Key? key,
+//     required this.color,
+//     required this.list,
+//     required this.index,
+//     required this.displayString,
+//   }) : super(key: key);
 
 //   @override
-//   State<SuggestionButton> createState() => _SuggestionButtonState();
+//   State<SuggestionButton> createState() => SuggestionButtonState();
 // }
 
-// class _SuggestionButtonState extends State<SuggestionButton> {
-//   double heightScaleFactor = 1 / 10;
-//   double suggestionWidthScaleFactor = 1 / 6;
-
+// class SuggestionButtonState extends State<SuggestionButton> {
 //   @override
 //   Widget build(BuildContext context) {
-//     return SizedBox(
-//       width: MediaQuery.of(context).size.width * (suggestionWidthScaleFactor),
-//       height: MediaQuery.of(context).size.height * (heightScaleFactor),
-//       child: TextButton(
-//           onPressed: () {},
-//           child: const Text(
-//             "",
-//             style: TextStyle(
-//                 color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 20),
-//           )),
-//     );
-//   }
-// }
-// // class SuggestionButton {
-// //   SuggestionButton();
+//     return TextButton(
+//       style: TextButton.styleFrom(
+//         backgroundColor: widget.color,
+//       ),
+//       onPressed: () {
+//         setState(() {
+//           widget.setDisplayString();
+//           widget.displayString + widget.list[widget.index];
+//         });
+//         print("suggestion button ${widget.index + 1} pressed");
+//       },
+//       child: Text(widget.list[widget.index]));
+// };
+
+
+
+
+
+// // import 'dart:ui';
+
+// // import 'package:flutter/material.dart';
+
+
+
+
+
+// // TextButton suggestionButton(
+// //     Color color, List<String> list, int index, String displayString) {
+// //   return TextButton(
+// //       style: TextButton.styleFrom(
+// //         backgroundColor: color,
+// //       ),
+// //       onPressed: () {
+// //         setState(() {
+// //           displayString = displayString + list[index];
+// //         });
+// //         print("suggestion button ${index + 1} pressed");
+// //       },
+// //       child: Text(list[index]));
 // // }
